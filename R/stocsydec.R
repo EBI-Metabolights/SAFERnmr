24548412b87a20a36a134f656aacb472dc964328
cmartin <cmartin@ebi.ac.uk>
2022-09-05 16:22:11 +0100

    added documentation to stocsydec.R

--- R/stocsydec.R
+++ R/stocsydec.R
@@ -1,4 +1,12 @@
-# Function to run 'STOCSY decomposition' Goncalo Graca, 15 September 2020, g.gomes-da-graca@imperial.ac.uk
+#' stocsydec
+#'
+#' Function to run 'STOCSY decomposition' Goncalo Graca, 15 September 2020, g.gomes-da-graca@imperial.ac.uk
+#' 
+#' @param spectra individual spectra file to run through decomp
+#' @param idx index number of spextra files to be run through decomp
+#' @param cthr cutoff threshold
+#' @return Result of STOCS decomposition.
+#' @export
 stocsydec <- function(spectra, idx, cthr = 0.8) {
     ppm <- spectra[1, ]
     c <- cor(spectra[-1, ], spectra[-1, idx])
