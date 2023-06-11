5d9977eccfc0b373e6cae9520a7afa56764892f2
Michael T. Judge <judgemt@uga.edu>
2023-06-11 13:47:50 +0100

    running locally. create tmp directory if not present. runner script now just 3 lines.

--- slimrunner.R
+++ slimrunner.R
@@ -1,11 +1,3 @@
-setwd('/Users/mjudge/Documents/GitHub/icl_nmr_R')
-devtools::document()
-
-setwd('/Users/mjudge/Documents/not_galaxy')
+devtools::document('/Users/mjudge/Documents/GitHub/icl_nmr_R')
 pipeline(params_loc = '/Users/mjudge/Documents/mtbls430/430.params.yaml')
-
-run_params <- '/Users/mjudge/Documents/current_run/params.yaml'
-pars <- yaml::yaml.load_file(run_params, eval.expr = TRUE)
-
-devtools::document()
-show.me.the.evidence(results.dir = '/Users/mjudge/Documents/current_run')
+show.me.the.evidence(results.dir = '/Users/mjudge/Documents/not_galaxy')
