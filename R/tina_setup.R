4ab36125ebeeacbeb46420426c3deb181bdadfd5
Michael T. Judge <judgemt@uga.edu>
2023-08-03 18:31:37 +0100

    Update tina_setup.R
    much of setup unnecessary now (e.g. region intersections, etc.)
    added error checking

--- R/tina_setup.R
+++ R/tina_setup.R
@@ -27,80 +27,40 @@
 tina_setup <- function(a, xmat){
       
       message("TINA Setup...")
-    
-      # Build a matrix to house all the features
-        # Extract final region inds with regard to peak
-          relinds <- lapply(1:length(a),
-                            function(x) a[[x]]$finalRegion %in% a[[x]]$ref.idx %>% which)
-          # an error (which -> %in%) would imply the finalRegion does not contain the peak.
-          # Error in a[[x]]$finalRegion : $ operator is invalid for atomic vectors:
-          # - a is not what you think it is. Last time this failed because of next_driver()
-          # indexing failing due to NAs in ref.idx (fixed). 
 
-          featureStack <- relinds %>% unlist %>% max %>% matrix(NA, nrow = length(a), ncol = .)
-          positionStack <- featureStack
-          subsets <- matrix(FALSE, nrow(featureStack), nrow(xmat))
+      # Build matrices to house all the features and positions
+        # Extract region inds for each feature
+          relinds <- lapply(1:length(a), function(x){
+            tryCatch(
+              expr = {
+                       a[[x]]$finalRegion %in% a[[x]]$ref.idx %>% which
+                
+              }, error = function(cond) NULL)
+          })
+
+        # Build the matrices
+          v <- rep(NA, relinds %>% unlist %>% max)
           
-          # Get the driver within the feature
-              # browser()
-              # drivers.relative <- lapply(a, function(x) which(x$ref.idx == x$peak)) %>% unlist
+          featureStack <- lapply(1:length(a), function(i){
+            v[relinds[[i]]] <- a[[i]]$ref.vals
+            return(v)
+          }) %>% do.call(rbind,.)
 
+          positionStack <- lapply(1:length(a), function(i){
+            v[relinds[[i]]] <- a[[i]]$ref.idx
+            return(v)
+          }) %>% do.call(rbind,.)
+               
+          v <- rep(FALSE, nrow(xmat))
+          subsets <- lapply(1:length(a), function(i){
+            v[a[[i]]$subset] <- TRUE
+            return(v)
+          }) %>% do.call(rbind,.)
           
-        # Fill in the values
-          for (i in 1:length(a)){
-            featureStack[i,relinds[[i]]] <- a[[i]]$ref.vals
-            positionStack[i,relinds[[i]]] <- a[[i]]$ref.idx
-            subsets[i, a[[i]]$subset] <- TRUE
-          }
+          subset.sizes <- subsets %>% rowSums
 
-        # Look at the subset overlap
-                  # rowSums(subsets) %>% plot(xlab = "Feature (sorted)",
-                  #                                    ylab = "Subset size")
-                  # title(main = "Subset Size Distribution pre-afc")
-                  # abline(h = 5)
-          # subset.diffs <- subsets %>% diff %>% abs %>% rowSums
-          # subset.overlaps <- (subsets[-1,] & subsets[-nrow(subsets),]) %>% rowSums
-          # subset.sizes <- subsets %>% rowSums
-                  # (subset.diffs / subset.overlaps) %>% sort %>% plot(xlab = "Feature Comparison",
-                  #                                                    ylab = "Subsets diff/overlap")
-                  # subset.ratios <- (subset.diffs / subset.overlaps) 
-                  # subset.ratios[subset.ratios < .5] %>% sort %>% plot(xlab = "Feature Comparison",
-                  #                                                     ylab = "Subsets diff/overlap")
-                  
-          
-
-          # If the comparison is missing > 0.5 of the spectra in either subset
-            # subset.frac.1 <- subset.overlaps / subset.sizes[-nrow(subsets)]
-            # subset.frac.2 <- subset.overlaps / subset.sizes[-1]
-            # subset.frac <- subset.frac.1
-            # nds <- subset.frac.1 > subset.frac.2
-            # subset.frac[nds] <- subset.frac.2[nds] 
-            
-            
-            # subset.frac %>% sort %>% plot(xlab = "Feature Comparison",
-            #                               ylab = "Feature Subset Overlap (lesser)")
-            #   abline(h = 0.5, col = "red")
-              
-        # Look at the position region overlap (speed up later)
-            # reg.frac <- rep(0, nrow(positionStack)-1)
-            # reg.overlap <- reg.frac
-            # for (i in 1:length(reg.frac)){
-            #   r1 <- positionStack[i,] %>% range(na.rm = TRUE) %>% fillbetween
-            #   r2 <- positionStack[i+1,] %>% range(na.rm = TRUE) %>% fillbetween
-            #   reg.overlap[i] <- sum(r1 %in% r2)
-            #   reg.frac.1 <- reg.overlap[i] / length(r1)
-            #   reg.frac.2 <- reg.overlap[i] / length(r2)
-            #   reg.frac[i] <- min(c(reg.frac.1, reg.frac.2))
-            # }
-            # reg.sizes <- lapply(relinds, length) %>% unlist
-            #   
-            # # reg.frac %>% sort %>% plot(xlab = "Feature Comparison",
-            # #                            ylab = "Feature Region Overlap (lesser)")
-            # #   abline(h = 0.5, col = "red")
-            # 
-        # Get relative driver
-            # print(length(a))
-            
+        # Get relative driver within feat
+        
             drivers.relative <- lapply(1:length(a), function(x) {
               
               if (is.null(a[[x]]$peak)){
@@ -121,13 +81,7 @@ tina_setup <- function(a, xmat){
     return(list(stack = featureStack,
                 position = positionStack,
                 subset = list(ss.all = subsets,
-                              diffs = subset.diffs,
-                              sizes = subset.sizes,
-                              overlaps = subset.overlaps,
-                              fraction = subset.frac),
-                # region = list(overlaps = reg.overlap,
-                #               fraction = reg.frac,
-                #               sizes = reg.sizes),
+                              sizes = subset.sizes),
                 driver.relative = drivers.relative
                 )
            )
