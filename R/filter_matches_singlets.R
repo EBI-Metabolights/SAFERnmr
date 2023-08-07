bf98385e5ad360fbb9f00d61975a78c671f0afec
Michael T. Judge <judgemt@uga.edu>
2023-08-07 11:50:45 +0100

    Update filter_matches_singlets.R
    - tryCatch the main loop body
    - use compressed matrices for feature and ref stacks
    - apply_fit also using compressed scheme

--- R/filter_matches_singlets.R
+++ R/filter_matches_singlets.R
@@ -33,6 +33,16 @@ filter_matches_singlets <- function(match.info,
       #   source('./../ind2subR.R')
       #   source('./../stackplot.R')
       
+      # Define function to create dummy vals for row if failed:
+        failed <- function(mi){
+          mi$numpeaks.feat <- 0
+          mi$numpeaks.ref <- 0
+          mi$numpeaks.feat.nnf <- 0
+          mi$refpeaks.matched <- 0
+          mi
+        }      
+        
+      # Loop through each row of match.info, recreate the fit vectors, then characterize peaks 
         message('filtering out matches:',
                   '\n\t- for which the fit feature has 1 or fewer resonances...',
                   '\n\t- involving ref features with 1 or fewer resonances...',
@@ -40,45 +50,71 @@ filter_matches_singlets <- function(match.info,
                   '\n\t- for which 1 or fewer resonances had at least ', res.area.threshold, 
                ' of their area explained by the matching feature...')
         
-        browser()
+        
+        f.cstack <- compress_stack(feature.stack)
+        r.cstack <- compress_stack(t(ref.mat)) # refs on rows
+
       # Apply singlet filters to each mi row, and add the results as fields. 
-        # match.info <- mclapply(1:nrow(match.info), 
-        match.info <- lapply(1:nrow(match.info), 
+        match.info <- mclapply(1:nrow(match.info),
+        # match.info <- lapply(1:100,#nrow(match.info), 
                                function(m){
-          print(m)
+          # m <- 1                       
+          # print(m)
           
-          ########### singlet filter for fit features ################
             mi <- match.info[m, ]
             
-            ff <- apply_fit(mi.row = mi, feat.stack = feature.stack, ref.stack = ref.mat)
-            
-            mask <- !is.na(ff$residuals)
-            mi$numpeaks.feat <- pk_maxs(ff$feat.fit, mask) %>% length
-          
-          ########### singlet filter for fit ref regions ################
-          
-            mi$numpeaks.ref <- pk_maxs(ff$spec.fit, mask) %>% length
-          
-          ########### singlet filter for feature-not-never-fit regions ################
-            
-            peak.quality <- peak.qualities[[which(match.info$feat[m] == pq.featureNumbers)]]
-            f.adj <- ff$feat.fit - ff$feat.fit * peak.quality
-            f.adj <- f.adj - min(f.adj, na.rm = T)
-            mi$numpeaks.feat.nnf <- pk_maxs(f.adj, mask = !is.na(f.adj)) %>% length # not sure if mask here is always the same as above
-          
-          ########### singlet filter for refpeaks.matched ################
-          
-            pks <- extractPeaks_corr(ff$spec.fit,plots = F)
-            pk.coverage <- lapply(pks$bounds[pks$truePeak], function(p){
-              pkinds <- p %>% unlist %>% fillbetween
-              return(sum(ff$feat.fit[pkinds],na.rm = T) / sum(ff$spec.fit[pkinds],na.rm = T))
-            })
-            mi$refpeaks.matched <- sum(pk.coverage > (1-res.area.threshold) & pk.coverage < (1+res.area.threshold))
+            # Wrap the whole thing in tryCatch; if any of the peak extraction fails, the row will be filtered out anyways.
+            mi <- tryCatch(
+              expr = {
+              ########### singlet filter for fit features ################
+                
+                ff <- apply_fit(mi.row = mi, feat.stack = f.cstack, ref.stack = r.cstack)
+                
+                mask <- !is.na(ff$feat.fit + ff$spec.fit)
+                  if (length(mask) == 0){return(failed(mi))}
+                
+                mi$numpeaks.feat <- pk_maxs(ff$feat.fit, mask) %>% length
+              
+              ########### singlet filter for fit ref regions ################
+              
+                mi$numpeaks.ref <- pk_maxs(ff$spec.fit, mask) %>% length
+              
+              ########### singlet filter for feature-not-never-fit regions ################
+                
+                peak.quality <- peak.qualities[[which(mi$feat == pq.featureNumbers)]]
+                f.adj <- ff$feat.fit - ff$feat.fit * peak.quality
+                # f.adj <- f.adj - min(f.adj, na.rm = T)
+                mi$numpeaks.feat.nnf <- pk_maxs(f.adj, mask = !is.na(f.adj)) %>% length # not sure if mask here is always the same as above
+              
+              ########### singlet filter for refpeaks.matched ################
+              
+                pks <- extractPeaks_corr(ff$spec.fit,plots = F)
+                pk.coverage <- lapply(pks$bounds[pks$truePeak], function(p){
+                  pkinds <- p %>% unlist %>% fillbetween
+                  return(sum(ff$feat.fit[pkinds],na.rm = T) / sum(ff$spec.fit[pkinds],na.rm = T))
+                })
+                mi$refpeaks.matched <- sum(pk.coverage > (1-res.area.threshold) & pk.coverage < (1+res.area.threshold))
+    
+              ########### Return row #########
+              
+                return(mi)
+                
+              }, 
+              
+              # Or, if failed at any point, simply return NAs
+              warning = function(cond){
+                
+                return(failed(mi))
+                
+              }, 
+              error = function(cond){
+                
+                return(failed(mi))
+                
+              }
+            )
 
-          ########### Return row #########
-          return(mi)
-            
-        }#, mc.cores = ncores
+        }, mc.cores = ncores
         )
         
       # Rbind results
@@ -96,3 +132,6 @@ filter_matches_singlets <- function(match.info,
       return(match.info)
         
 }
+
+
+
