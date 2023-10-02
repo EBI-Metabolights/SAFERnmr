e27fd1f5b06fb37af4ae02ffe36b0a0f37ad4a00
Michael T. Judge <judgemt@uga.edu>
2023-10-02 11:10:16 +0100

    Update figures.R

--- figures.R
+++ figures.R
@@ -37,33 +37,49 @@ feat.span <- span(cols)
 reg <- feature$position[ind, cols] %>% expand_window(within = c(1,length(ppm)), by = feat.span)
 
 xmat[, reg] %>% stackplot(xvect = ppm[reg], vshift = 3)
+
 s=fse.result$storm_features[[a.ind]]
 driver <- s$peak
 which(reg %in% s$peak)
 
+
       specRegion <- xmat[, reg] %>% scale_between
       scale.to <- specRegion %>% Rfast::colMaxs(value = TRUE)
       
-      
       sr <- list(
         cor = cor(specRegion, xmat[, driver]),
-        cov = cor(specRegion, xmat[, driver]) %>% scale_between# %>% fit_batman(scale.to, exclude.lowest = 0.5) %>% .[["feat.fit"]]
+        cov = cor(specRegion, xmat[, driver]) %>% scale_between # %>% fit_batman(scale.to, exclude.lowest = 0.5) %>% .[["feat.fit"]]
       )
 
-      g <- simplePlot(specRegion, ppm[reg], n_xticks = 4)
-      # g <- stackplot(specRegion, ppm[reg], vshift = 1, hshift = 0)
-      
-      # # Make the plot
-      #   g <- g + geom_path(data = data.frame(vals = sr$cov,
-      #                                        ppms = ppm[reg]),
-      #                      mapping = aes(x = ppms, y = vals),
-      #                      colour = "black", linewidth = .5,
-      #                      lineend = "round",
-      #                      linejoin = "round",
-      #                      linemitre = "5")
+      specRegion[s$subset, ]  # pull subset
+      specRegion[sortorder, ] # reorder rows low to high
 
-      # Plot the stocsy line over the current plot
+      # Stackplot
+      
+        g <- specRegion %>%
+          stackplot(ppm[reg], vshift = .3, hshift = 0)
         
+        pdf(file = '/Users/mjudge/Dropbox (Edison_Lab@UGA)/MJ_UGA_Root/Scheduling/citrate_peak_stackplot.pdf', width = 12, height = 6)
+            g
+        dev.off()
+      
+            
+      # Sorted stackplot with highlighted subset
+      
+        sortorder <- specRegion %>% rowSums(na.rm = TRUE) %>% order
+        ss.sorted <- order(sortorder) %>% .[s$subset]
+          
+        g <- specRegion[sortorder,] %>%
+          stackplot(ppm[reg], vshift = .3, hshift = 0, highlight.spec = ss.sorted)
+        
+        pdf(file = '/Users/mjudge/Dropbox (Edison_Lab@UGA)/MJ_UGA_Root/Scheduling/citrate_peak_stackplot_selected.pdf', width = 12, height = 6)
+            g
+        dev.off()
+      
+      
+      # Plot the stocsy line over the simple overlay plot
+        
+        g <- simplePlot(specRegion, ppm[reg], n_xticks = 4)
         
         g <- g + new_scale_color() +
           geom_line(data = data.frame(refvals = sr$cov,
@@ -77,12 +93,35 @@ which(reg %in% s$peak)
           scale_colour_gradientn(colours = matlab.like2(10),
                                  limits = c(-1, 1))
           
+        
       # Plot corr boundaries as vert lines  
+        
         g <- g +
           geom_vline(xintercept = ppm[s$peak], linetype = 2, col = "black")
-
-        g
-
-  plot_storm_refRegions(xmat = xmat, s = s, ppm = ppm, calcStocsy = F) +
+        
+        pdf(file = '/Users/mjudge/Dropbox (Edison_Lab@UGA)/MJ_UGA_Root/Scheduling/citrate_peak_stocsy.pdf', width = 12, height = 6)
+          g
+        dev.off()
+        
+        
+        s$finalRegion <- reg
+        sr <- list(
+          cor = cor(specRegion[s$subset, ], xmat[s$subset, driver]),
+          cov = cor(specRegion[s$subset, ], xmat[s$subset, driver]) %>% scale_between # %>% fit_batman(scale.to, exclude.lowest = 0.5) %>% .[["feat.fit"]]
+        )
+        sr$cor[sr$cor < 0.8] <- NA
+        sr$cov[sr$cov < 0.8] <- NA
+        s$corr <- sr$cor
+        s$covar <- sr$cov
+        devtools::document('/Users/mjudge/Documents/GitHub/SAFER')
+        g <- plot_storm_refRegions(xmat = xmat, s = s, ppm = ppm, calcStocsy = F, xlim = range(reg) %>% ppm[.]) +
           geom_vline(xintercept = ppm[s$peak], linetype = 2, col = "black")
+        
+        pdf(file = '/Users/mjudge/Dropbox (Edison_Lab@UGA)/MJ_UGA_Root/Scheduling/citrate_peak_storm.pdf', width = 12, height = 6)
+          g
+        dev.off()
+
+
+
+
  
