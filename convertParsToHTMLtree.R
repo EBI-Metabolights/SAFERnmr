7cbfdf4f6b740a6d3af2e69a825860b5f804fd72
Michael T. Judge <judgemt@uga.edu>
2023-10-26 18:01:19 +0100

    Create convertParsToHTMLtree.R
    GPT-generated, MTJ-modified function for recursing through a nested parameters list and generating Github-friendly html for expandable markup sections.

---
+++ convertParsToHTMLtree.R
@@ -0,0 +1,34 @@
+# thanks, GPT!
+
+nestedListToHTML <- function(lst, indent = 0, newline = TRUE) {
+  html_string <- ""
+  for (key in names(lst)) {
+    html_string <- paste0(html_string, strrep(" ", indent), "<details>\n")
+    html_string <- paste0(html_string, strrep(" ", indent), "<summary>", key, "</summary>\n")
+    html_string <- paste0(html_string, strrep(" ", indent), "<blockquote>\n")
+
+    if (is.list(lst[[key]])) {
+      html_string <- paste0(html_string, nestedListToHTML(lst[[key]], indent+1, newline))
+    } else {
+      for (element in lst[[key]]) {
+        html_string <- paste0(html_string, strrep(" ", indent+1), element, "<br>\n")
+      }
+    }
+    html_string <- paste0(html_string, strrep(" ", indent), "</blockquote>\n")
+    html_string <- paste0(html_string, strrep(" ", indent), "</details>\n")
+    if (newline) {
+      html_string <- paste0(html_string, "\n")
+    }
+  }
+  return(html_string)
+}
+
+
+
+
+# txt <- nestedListToHTML(pars)
+# Convert the nested list to HTML with proper indentation and newline characters
+html_output <- nestedListToHTML(pars, newline = TRUE)
+
+# Writing the HTML output to a file
+writeLines(html_output, "/Users/mjudge/Documents/GitHub/SAFER/pars.html")
