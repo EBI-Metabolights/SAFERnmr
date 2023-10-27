506bed287e9cc547f4f2b42f32f8d41776a2fa17
Michael T. Judge <judgemt@uga.edu>
2023-10-27 09:09:17 +0100

    useful tools for making wiki page
    parameters object -> foldable/expandable markup file

--- convertParsToHTMLtree.R
+++ convertParsToHTMLtree.R
@@ -23,6 +23,36 @@ nestedListToHTML <- function(lst, indent = 0, newline = TRUE) {
   return(html_string)
 }
 
+nestedListToHTML <- function(lst, indent = 0, newline = TRUE) {
+  html_string <- ""
+  for (key in names(lst)) {
+    html_string <- paste0(html_string, strrep(" ", indent), "<details>\n")
+    html_string <- paste0(html_string, strrep(" ", indent), "<summary>", key, "</summary>\n")
+    html_string <- paste0(html_string, strrep(" ", indent), "<blockquote>\n")
+
+    if (is.list(lst[[key]])) {
+      html_string <- paste0(html_string, nestedListToHTML(lst[[key]], indent + 1, newline))
+    } else {
+      for (element in lst[[key]]) {
+        html_string <- paste0(html_string, strrep(" ", indent + 1), element, "<br>\n")
+      }
+    }
+
+    html_string <- paste0(html_string, strrep(" ", indent + 1), "<details>\n")
+    html_string <- paste0(html_string, strrep(" ", indent + 1), "<summary>description</summary>\n")
+    html_string <- paste0(html_string, strrep(" ", indent + 1), "<blockquote>\n")
+    html_string <- paste0(html_string, strrep(" ", indent + 2), "description<br>\n")
+    html_string <- paste0(html_string, strrep(" ", indent + 1), "</blockquote>\n")
+    html_string <- paste0(html_string, strrep(" ", indent + 1), "</details>\n")
+
+    html_string <- paste0(html_string, strrep(" ", indent), "</blockquote>\n")
+    html_string <- paste0(html_string, strrep(" ", indent), "</details>\n")
+    if (newline) {
+      html_string <- paste0(html_string, "\n")
+    }
+  }
+  return(html_string)
+}
 
 
 
